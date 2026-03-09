edbd2c6e475f8954409a20907b200411e68142be
Moshiour45 <moshiourrahman145@gmail.com>
2026-03-09 16:24:20 +0600

    Header, Hero Section & Footer is added.

---
+++ script.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById('menu-toggle');
+const menu = document.getElementById('mobile-menu');
+btn.addEventListener('click', () => {
+    menu.classList.toggle('open');
+});
